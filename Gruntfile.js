2898ec8b1ad3720666c395165c43c65e4591ed47
veit98 <veit.schumacher@gmail.com>
2019-06-17 19:46:57 +0200

    Adds Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';\n',
+        sourceMap: true
+      },
+      dist: {
+        src: 'js/src/*.js',
+        dest: 'js/dist/<%= pkg.name %>.js'
+      }
+    },
+    watch: {
+      scripts: {
+        files: 'js/src/*.js',
+        tasks: ['concat'],
+        options: {
+          atBegin: true
+        }
+      }
+    },
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
+  });
+
+  // Load uglify task
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load concat task
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  // Load watch task
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
