e1a4e805bb5a0550c044b78c2fd88a2150187743
vs1998 <veit.schumacher@gmail.com>
2019-06-27 17:03:19 +0200

    Add some animation test

--- js/src/start.js
+++ js/src/start.js
@@ -1,5 +1,5 @@
 import PokerGameController from './controller/PokerGameController.js';
-import {communityCardPosition} from './model/Utils.js';
+import {communityCardPosition, PLAYER_POSITION,CARD_SUIT,CARD_VALUE} from './model/Utils.js';
 
 let pokerController = new PokerGameController();
 
@@ -12,6 +12,32 @@ for(let i = 0; i < 5; i++) {
     Math.floor(Math.random() * 13) + 2
   );
 }
+
+pokerController.addCardWithMoves(
+  PLAYER_POSITION[0].cards[0](pokerController.gameCanvas).x,
+  PLAYER_POSITION[0].cards[0](pokerController.gameCanvas).y,
+  PLAYER_POSITION[0].cardRotation,
+  CARD_SUIT.HEARTS,
+  CARD_VALUE.KING,
+  [
+    {
+      xd: PLAYER_POSITION[3].cards[0](pokerController.gameCanvas).x,
+      yd: PLAYER_POSITION[3].cards[0](pokerController.gameCanvas).y,
+      rotation: 180,
+      easing: 'ease-in',
+      time: 1000,
+      delay: 1000,
+    },
+    {
+      xd: PLAYER_POSITION[5].cards[0](pokerController.gameCanvas).x,
+      yd: PLAYER_POSITION[5].cards[0](pokerController.gameCanvas).y,
+      rotation: 180,
+      easing: 'ease-in',
+      time: 1000,
+      delay: 2000,
+    },
+  ]
+);
 
 
 pokerController.start();
