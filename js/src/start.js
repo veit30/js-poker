b39805ed6d26196e2f0ef4b2fb8913f223b0c095
veit98 <veit.schumacher@gmail.com>
2019-06-30 20:13:08 +0200

    Clean up start.js.

--- js/src/start.js
+++ js/src/start.js
@@ -3,16 +3,10 @@ import {communityCardPosition, PLAYER_POSITION,CARD_SUIT,CARD_VALUE} from './mod
 
 let pokerController = new PokerGameController();
 
-for(let i = 0; i < 5; i++) {
-  pokerController.addCard(
-    communityCardPosition(pokerController.tableCanvas,i).x,
-    communityCardPosition(pokerController.tableCanvas,i).y,
-    0,
-    Math.floor(Math.random() * 4) + 1,
-    Math.floor(Math.random() * 13) + 2
-  );
-}
+pokerController.startGame();
+pokerController.start();
 
+/*
 pokerController.addCardWithMoves(
   PLAYER_POSITION[0].cards[0](pokerController.gameCanvas).x,
   PLAYER_POSITION[0].cards[0](pokerController.gameCanvas).y,
@@ -38,6 +32,7 @@ pokerController.addCardWithMoves(
     },
   ]
 );
+*/
 
 
 pokerController.start();
