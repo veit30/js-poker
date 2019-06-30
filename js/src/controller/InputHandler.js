896471251e4d7d94cc912dc7932ba6005434a934
veit98 <veit.schumacher@gmail.com>
2019-06-30 20:04:45 +0200

    Add key press listener in input handler.

--- js/src/controller/InputHandler.js
+++ js/src/controller/InputHandler.js
@@ -1,5 +1,22 @@
 export default class InputHandler {
   constructor() {
+    this.pressed = {};
 
+    document.addEventListener('keydown', (event) => {
+      event.preventDefault();
+    }, false);
+
+    document.addEventListener('keyup', (event) => {
+      this.pressed[event.keyCode] = true;
+      event.preventDefault();
+    }, false);
   }
+
+  askKeyPress(keyCode) {
+    if (this.pressed[keyCode] !== undefined) {
+      delete this.pressed[keyCode];
+      return true;
+    }
+  }
+
 }
