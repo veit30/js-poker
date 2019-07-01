0b0ddd3dde1fed55d30819744f07f3904d9efd28
vs1998 <veit.schumacher@gmail.com>
2019-07-01 16:58:02 +0200

    Add new class Button for buttons.

---
+++ js/src/model/Button.js
@@ -0,0 +1,18 @@
+export default class Button {
+  constructor(x,y,height,width,color) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.color = color;
+  }
+
+  intersect({x,y}) {
+    if (x >= (this.x - this.width * .5) && x <= (this.x + this.width * .5)) {
+      if(y >= (this.y - this.height * .5) && y <= (this.y + this.height * .5)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
