ecbdc70542cc5d7e892ac06b092b252b87fca3b3
vs1998 <veit.schumacher@gmail.com>
2019-07-01 16:57:20 +0200

    Add click functionality.

--- js/src/controller/InputHandler.js
+++ js/src/controller/InputHandler.js
@@ -1,15 +1,42 @@
 export default class InputHandler {
   constructor() {
     this.pressed = {};
+    this.cursor = {
+      x: 0,
+      y: 0
+    };
+    this.click = {
+      x:0,
+      y:0
+    }
 
-    document.addEventListener('keydown', (event) => {
+    this.clickActions = [];
+
+    window.addEventListener('keydown', event => {
       event.preventDefault();
     }, false);
 
-    document.addEventListener('keyup', (event) => {
+    window.addEventListener('keyup', event => {
       this.pressed[event.keyCode] = true;
       event.preventDefault();
     }, false);
+
+    window.addEventListener('mousemove', event => {
+      this.cursor.x = event.clientX;
+      this.cursor.y = event.clientY;
+    })
+
+    window.addEventListener('click', event => {
+      // this.click.x = event.clientX;
+      // this.click.y = event.clientY;
+      this.clickActions.forEach(a => {
+        a({x: event.clientX, y: event.clientY});
+      });
+    })
+  }
+
+  resetClick() {
+    this.click = {x:-1,y:-1};
   }
 
   askKeyPress(keyCode) {
@@ -17,6 +44,10 @@ export default class InputHandler {
       delete this.pressed[keyCode];
       return true;
     }
+  }
+
+  bindOnClickEvent(fn) {
+    this.clickActions.push(fn);
   }
 
 }
