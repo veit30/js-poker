ae392caff91f750871f6f7b857dc4d68b72c64f6
vs1998 <veit.schumacher@gmail.com>
2019-07-02 14:00:07 +0200

    Move alot to new Game class. Modified code to work with new implementation (not finished yet).

--- js/src/controller/PokerGameController.js
+++ js/src/controller/PokerGameController.js
@@ -17,20 +17,20 @@ export default class PokerGameController {
   constructor(options) {
 
     this.options = options;
-    this.players = [];
-    this.cards = [];
-    this.communityCards = [];
     this.chips = [];
     this.tableCanvas;
     this.gameCanvas;
     this.inputCanvas;
+    /*
     this.game = {
       round: 0,
       state: 'menu',
       pot: 10000,
       potChanged: true
     };
-    this.buttons = [];
+    */
+    this.game;
+    this.renderState = 'menu';
 
     this.inputHandler = new InputHandler();
     this.setupCanvas();
@@ -71,33 +71,6 @@ export default class PokerGameController {
     });
   }
 
-  generateCards() {
-    let card;
-    for (let suit of Object.keys(CARD_SUIT)) {
-      for (let value of Object.keys(CARD_VALUE)) {
-        card = new Card(
-          this.gameCanvas.width * .5,
-          this.gameCanvas.height * .5 - (this.gameCanvas.width * .4) * .35,
-          0,
-          CARD_SUIT[suit],
-          CARD_VALUE[value]
-        );
-        this.objectController.calcRelPosProp(card);
-        this.cards.push(card);
-      }
-    }
-  }
-
-  shuffleCards() {
-    for (let i = 0, len = this.cards.length; i < len; i++) {
-      let randomIndex = Math.floor(Math.random() * len);
-      let tmpCard1 = this.cards[randomIndex];
-      let tmpCard2 = this.cards[i];
-      this.cards[i] = tmpCard1;
-      this.cards[randomIndex] = tmpCard2;
-    }
-  }
-
   updateCanvas() {
     this.tableCanvas.width = this.tableCanvas.clientWidth;
     this.tableCanvas.height = this.tableCanvas.clientHeight;
@@ -105,11 +78,6 @@ export default class PokerGameController {
     this.gameCanvas.height = this.gameCanvas.clientHeight;
     this.inputCanvas.width = this.inputCanvas.clientWidth;
     this.inputCanvas.height = this.inputCanvas.clientHeight;
-  }
-
-  addChip(value) {
-    let chip = new Chip(200,200,0,value,Color.red,this.ctx);
-    this.chips.push(chip);
   }
 
   renderGameObjects() {
@@ -153,21 +121,15 @@ export default class PokerGameController {
       this.objectController.windowResized = false;
     }
   }
-
+  // testing
   startGame() {
-    this.addPlayer('John',3);
-    this.addPlayer('Adam',4);
-    this.addPlayer('Flavio',6);
-    this.addPlayer('Ron',7);
-    this.generateCards();
-    this.shuffleCards();
-    this.dealCards();
-    this.game.round = 1;
-  }
-
-  addPlayer(name,id) {
-    let player = new Player(name,id)
-    this.players.push(player);
+    this.game = new Game(this.gameCanvas);
+    this.game.addPlayer(new Player('John',3,283791))
+    this.game.addPlayer(new Player('Adam',4,832374))
+    this.game.addPlayer(new Player('Daniel',5,832947))
+    this.game.addPlayer(new Player('Ive',6,908732))
+    this.game.addPlayer(new Player('Holg',7,932874))
+    this.game.startNewGame();
   }
 
   // basic game loop function
@@ -175,6 +137,7 @@ export default class PokerGameController {
     this.inputView.renderMenu();
     this.game.state = this.inputView.state;
     if (this.game.state === 'ingame') {
+      // Keyboard inputs are only used for testing purposes for now
       if (this.inputHandler.askKeyPress(KEY.C)) {
         this.flopTurnRiver();
         this.game.round++;
@@ -187,17 +150,9 @@ export default class PokerGameController {
         this.game.pot--;
         this.game.potChanged = true;
       }
-      if (this.game.potChanged) {
-        this.tableView.renderTable();
-        let potTextStr = numDots(this.game.pot);
-        let potText = new Text(numDots(this.game.pot),this.inputCanvas.width * .03,FONT.MAIN,undefined,undefined,'left');
-        let width = potText.calcWidth(this.tableCanvas.getContext('2d'));
-        this.tableView.renderText(
-          this.inputCanvas.width * .5 + ((this.inputCanvas.width * .08) - width * 1.1),
-          this.inputCanvas.height *.5 - this.inputCanvas.width * .054,
-          potText
-        );
-        this.game.potChanged = false;
+      if (this.game.hasNewPot()) {
+        this.displayPotSize();
+        this.game.noticedPotSize();
       }
     }
 
@@ -206,51 +161,52 @@ export default class PokerGameController {
     this.raf = requestAnimationFrame(() => this.start());
   }
 
-  flopTurnRiver() {
-    let card,index = [];
-    if (this.game.round === 1 && this.communityCards.length < 3) {
-      index = [0,1,2];
-    } else if (this.game.round === 2 && this.communityCards.length < 4) {
-      index = [3];
-    } else if (this.game.round === 3 && this.communityCards.length < 5) {
-      index = [4];
-    }
-
-    for (let i of index) {
-      card = this.cards.shift();
-      this.objectController.addMove(card,{
-        xd: communityCardPosition(this.gameCanvas,i).x,
-        yd: communityCardPosition(this.gameCanvas,i).y,
-        rotation: 0,
-        easing: 'ease-out',
-        time: 500,
-        delay: 10,
-        flipAfter: true
-      })
-      this.communityCards.push(card);
-    }
-
-    console.log(this.communityCards.length)
+  displayPotSize() {
+    this.tableView.renderTable();
+    let potText = new Text(this.game.potStr,this.inputCanvas.width * .03,FONT.MAIN,undefined,undefined,'left');
+    let width = potText.calcWidth(this.inputCanvas.getContext('2d'));
+    this.tableView.renderText(
+      this.inputCanvas.width * .5 + ((this.inputCanvas.width * .08) - width * 1.15),
+      this.inputCanvas.height *.5 - this.inputCanvas.width * .054,
+      potText
+    );
   }
 
-  dealCards() {
+
+  moveCommunityCards() {
+    this.game.communityCards.forEach(card => {
+      if (card.doneMoves == 0) {
+        this.objectController.addMove(card,{
+          xd: communityCardPosition(this.gameCanvas,i).x,
+          yd: communityCardPosition(this.gameCanvas,i).y,
+          rotation: 0,
+          easing: 'ease-out',
+          time: 500,
+          delay: 10,
+          flipAfter: true
+        })
+      }
+    })
+  }
+
+  movePlayerCards() {
     let delay = 500;
     let card;
+
     for(let i = 0; i < 2; i++) {
-      for (let p = 0; p < this.players.length; p++) {
-        card = this.cards.shift();
+      this.game.players.forEach(player => {
+        card = player.cards[i];
         this.objectController.addMove(card,{
-          xd: playersCardPosition(this.players[p].id,i,this.gameCanvas).x,
-          yd: playersCardPosition(this.players[p].id,i,this.gameCanvas).y,
-          rotation: playersCardRotation(this.players[p].id),
+          xd: playersCardPosition(player.positionId,i,this.gameCanvas).x,
+          yd: playersCardPosition(player.positionId,i,this.gameCanvas).y,
+          rotation: playersCardRotation(player.positionId),
           easing: 'ease-out',
           time: 500,
           delay: delay,
           flipAfter: true
         })
         delay += 500;
-        this.players[p].cards.push(card);
-      }
+      })
     }
   }
 
